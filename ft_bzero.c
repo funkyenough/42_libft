87a5f8908ad50d4d329e327b0db6b895818bf412
root <root@fun.localdomain>
2024-04-07 13:05:39 +0900

    fix bzero, add strcat

--- ft_bzero.c
+++ ft_bzero.c
@@ -14,20 +14,34 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	int		i;
+	size_t	i;
 	char	*str;
 
+	str = (char *)s;
 	i = 0;
 	while (i < n)
-	{
-		str[i] = 0;
-		i++;
-	}
-	s = str;
+		str[i++] = '\0';
 }
 
-// int	main(void)
-// {
-// 	char *str = "My Name is Ying.";
-// 	ft_bzero(str);
-// }
+#include <stdio.h>
+
+int	main(void)
+{
+	char str[] = "My Name is Ying.";
+	size_t i;
+
+	printf("Before bzero:\n");
+	for (i = 0; i < sizeof(str); i++)
+	{
+		printf("%02x ", (unsigned char)str[i]);
+	}
+	printf("\n");
+	ft_bzero(str, 10);
+	printf("After bzero:\n");
+	for (i = 0; i < sizeof(str); i++)
+	{
+		printf("%02x ", (unsigned char)str[i]);
+	}
+	printf("\n");
+	return (0);
+}
---
+++ ft_strcat.c
@@ -0,0 +1,22 @@
+#include <string.h>
+
+char	*ft_strcat(char *dest, const char *src)
+{
+	char	*ptr;
+
+	ptr = dest;
+	while (*src)
+		*ptr++ = *src++;
+	*ptr = '\0';
+	return (dest);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char src[] = "Hello, World!";
+	char dest[15] = "abcdef";
+	printf("ft_cat: %s\n", ft_strcat(dest, src));
+	printf("strcat: %s\n", strcat(dest, src));
+}
