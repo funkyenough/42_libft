883c7bc662525f809fd21f50382252e466e623eb
Ying Hong <ying@Yings-MBP.local>
2024-04-17 12:09:48 +0900

    fix substr

--- ft_substr.c
+++ ft_substr.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yinhong <yinhong@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 12:29:12 by yinhong           #+#    #+#             */
-/*   Updated: 2024/04/16 18:01:33 by yinhong          ###   ########.fr       */
+/*   Updated: 2024/04/17 06:59:26 by yinhong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #include "libft.h"
 
-// char	*ft_strsub(char const *s, unsigned int start, size_t len)
+// char	*ft_substr(char const *s, unsigned int start, size_t len)
 // {
 // 	char *str;
 // 	char *ptr;
