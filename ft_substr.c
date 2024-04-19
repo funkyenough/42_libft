2bca37ce34247e67f380abb2d38850b9c6149ae4
funkyenough <funky.enough.jp@gmail.com>
2024-04-19 17:57:43 +0900

    fix substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: yinhong <yinhong@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 12:29:12 by yinhong           #+#    #+#             */
-/*   Updated: 2024/04/19 17:02:56 by yinhong          ###   ########.fr       */
+/*   Updated: 2024/04/19 17:55:38 by yinhong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*str;
 	char	*ptr;
 
+	if (start >= ft_strlen(s) || s == NULL)
+		return (ft_strdup(""));
+	if (ft_strlen(s + start) < len)
+		len = ft_strlen(s + start);
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (str == NULL || s == NULL)
 		return (NULL);
 	ptr = str;
-	if (ft_strlen(s + start) < len)
-		len = ft_strlen(s + start);
 	if (start < ft_strlen(s))
 	{
 		while (--len + 1)
